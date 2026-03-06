d04a28e4a20ce79635d94a57e1daf9bdeb9f2b85
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-03-06 21:02:48 +0600

    feat: Add todo.js and update projectManager.js
    - Added a new file `src/todo.js` to handle todo item functionalities.
    - Updated `src/projectManager.js` to integrate the new todo functionalities.

--- src/projectManager.js
+++ src/projectManager.js
@@ -1,4 +1,5 @@
 import createProject from "./project.js";
+import createTodo from "./todo.js";
 
 let projects = [{ id: "default", name: "default", todos: [] }];
 
@@ -51,4 +52,46 @@ export function getProjectById(projectId) {
   } else {
     throw new Error(`No project found with ID: ${projectId}`);
   }
+}
+
+export function addTodo(projectId, todoData) {
+  const project = getProjectById(projectId);
+  project.todos = [...structuredClone(project.todos), createTodo(todoData)];
+  projects = projects.map((p) => (p.id === projectId ? project : p));
+}
+
+export function getTodoById(projectId, todoId) {
+  const project = getProjectById(projectId);
+  let todo;
+
+  if (todoId) {
+    todo = project.todos.find((t) => t.id === todoId);
+  } else {
+    throw new Error("A todo ID must be provided!");
+  }
+
+  if (todo) {
+    return todo;
+  } else {
+    throw new Error(`No todo found with id ${todoId} in  ${project.name}`);
+  }
+}
+
+export function deleteTodo(projectId, todoId) {
+  const project = getProjectById(projectId);
+
+  if (getTodoById(projectId, todoId) && project.todos.length > 0) {
+    project.todos = project.todos.filter((t) => t.id !== todoId);
+    projects = projects.map((p) => (p.id === projectId ? project : p));
+  }
+}
+
+export function moveTodo(todoId, sourceProjectId, destinationProjectId) {
+  const todo = getTodoById(sourceProjectId, todoId);
+  deleteTodo(sourceProjectId, todoId);
+  addTodo(destinationProjectId, todo);
+}
+
+export function listTodos(projectId) {
+  return getProjectById(projectId).todos;
 }
---
+++ src/todo.js
@@ -0,0 +1,18 @@
+export default function createTodo({
+  title,
+  description,
+  dueDate,
+  priority,
+  notes = "",
+  checklist = [],
+}) {
+  return {
+    id: crypto.randomUUID(),
+    title,
+    description,
+    dueDate,
+    priority: "medium",
+    notes,
+    completed: false,
+  };
+}
