a6e62bdc8f57824fe48393fc72c13398c345a308
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-03-07 11:43:21 +0600

    Resume work on UI development
    - Renamed ui-backup to ui
    - Uncommented code in index.js to render the UI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import { getProjects } from "./projectManager.js";
 import "./styles/index.css";
-// import createSidebar from "./ui/sidebar.js";
+import createSidebar from "./ui/sidebar.js";
 
 const root = document.querySelector("#root");
 
-// createSidebar();
-
-// Test Codes:
-console.log("Initial projects: ", getProjects());
+createSidebar();
