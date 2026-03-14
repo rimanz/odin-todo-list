f98ed0da03eb6edf2d718355073b63c066106363
Riman Das <34216663+rimanz@users.noreply.github.com>
2026-03-15 00:18:37 +0600

    refactor: separate todo listing code from createProjectDetails function
    - Moved the code responsible for listing todos into a separate function
      called `listTodos` in the `projectDetails.js` file.

--- src/ui/projectDetails.js
+++ src/ui/projectDetails.js
@@ -11,12 +11,8 @@ const projectTitleEl = document.getElementById("project-view-header");
 const todoSectionEl = document.getElementById("todos");
 const todoListEl = document.getElementById("todo-list");
 
-export default function createProjectDetails() {
-  const activeProject = getProjectById(getActiveProjectId());
-  const todos = getTodos(activeProject.id);
-  const placeholderText = "For now there are no todos to show. Please add one.";
-
-  projectTitleEl.textContent = activeProject.name;
+export function listTodos(projectId) {
+  const todos = getTodos(projectId);
   todoListEl.textContent = "";
 
   if (todos.length > 0) {
@@ -33,4 +29,12 @@ export default function createProjectDetails() {
       parent: item,
     });
   }
+}
+
+export default function createProjectDetails() {
+  const activeProject = getProjectById(getActiveProjectId());
+  const placeholderText = "For now there are no todos to show. Please add one.";
+
+  projectTitleEl.textContent = activeProject.name;
+  listTodos(activeProject.id);
 }
